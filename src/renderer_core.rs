e6f2e29e6a891598fed5cfb2d0d3fde9c71e1cb6
Steve <stephenlclarke@mac.com>
2026-09-05 10:48:27 +0100

    fix(ci): support dependabot quality checks

--- src/renderer_core.rs
+++ src/renderer_core.rs
@@ -465,8 +465,10 @@ impl SceneRaster {
 
     pub fn is_non_blank(&self) -> bool {
         self.pixels
-            .chunks_exact(4)
-            .any(|pixel| pixel != [0, 0, 0, 255].as_slice())
+            .as_chunks::<4>()
+            .0
+            .iter()
+            .any(|pixel| pixel != &[0, 0, 0, 255])
     }
 }
 
